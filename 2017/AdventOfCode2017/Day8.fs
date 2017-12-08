d89465ff21a6d275d055f96c8562f097042cb2ba
Kenneth Sarmaat <knnth@outlook.com>
2017-12-08 11:27:52 +0100

    a little less mutation

--- 2017/AdventOfCode2017/Day8.fs
+++ 2017/AdventOfCode2017/Day8.fs
@@ -5,55 +5,58 @@
     open System.Collections.Generic
 
     type Condition =
-        { register: string; comparison: string; value: int }
+        { operand: string; comparison: string; value: int }
 
-    let registers = new Dictionary<string, int>()
+    type Instruction =
+        { register: string; increment: int; condition: Condition }
 
-    let setRegister name value =
-        registers.[name] <- value
+    let setRegister registers name value =
+        registers |> Map.add name value
 
-    let getRegister name =
-        match registers.TryGetValue(name) with
-        | true, n -> n
-        | _ -> 0
+    let getRegister registers name =
+        registers |> Map.tryFind name |> Option.defaultValue 0
 
     let parseLine (line:string) =
         let tokens = line.Split(' ')
         let reg = tokens.[0]
-        let value =
+        let increment =
             match tokens.[1] with
             | "inc" -> int tokens.[2]
             | "dec" -> - int tokens.[2]
             | _ -> 0
-        let condition = { register = tokens.[4]; comparison = tokens.[5]; value = int tokens.[6] }
+        let condition = { operand = tokens.[4]; comparison = tokens.[5]; value = int tokens.[6] }
 
-        reg, value, condition
+        { register = reg; increment = increment; condition = condition }
 
-    let conditionIsTrue condition =
-        let reg = getRegister condition.register
+    let conditionIsTrue registers condition =
+        let registerValue = getRegister registers condition.operand
         match condition.comparison with
-        | ">" -> reg > condition.value
-        | "<" -> reg < condition.value
-        | ">=" -> reg >= condition.value
-        | "<=" -> reg <= condition.value
-        | "==" -> reg = condition.value
-        | "!=" -> reg <> condition.value
+        | ">" -> registerValue > condition.value
+        | "<" -> registerValue < condition.value
+        | ">=" -> registerValue >= condition.value
+        | "<=" -> registerValue <= condition.value
+        | "==" -> registerValue = condition.value
+        | "!=" -> registerValue <> condition.value
         | _ -> false
 
-    let processLine (reg, value, condition) =
-        if conditionIsTrue condition
-        then setRegister reg (value + getRegister reg)
+    let highestValue =
+        Map.toList >> List.map snd >> List.max
 
-    let mutable highestIntermediate = new KeyValuePair<string,int>()
-
-    let storeHighest() =
-        let highest = registers |> Seq.maxBy (fun kv -> kv.Value)
-        if highest.Value > highestIntermediate.Value
-        then highestIntermediate <- highest
+    let processLine registers instruction highest =
+        if conditionIsTrue registers instruction.condition
+        then
+            let newValue = instruction.increment + getRegister registers instruction.register
+            let newRegisters = setRegister registers instruction.register newValue
+            let currentHighest = highestValue newRegisters
+            newRegisters, currentHighest, max highest currentHighest
+        else
+            registers, highestValue registers, highest
 
     let solve() =
-        File.ReadAllLines("..\..\Input\day8.txt")
-        |> Seq.iter (parseLine >> processLine >> storeHighest)
-        let highest = registers |> Seq.maxBy (fun kv -> kv.Value)
-        printfn "part 1 = %A" highest
-        printfn "part 2 = %A" highestIntermediate
+        let _, highest, totalHighest =
+            File.ReadAllLines("..\..\Input\day8.txt")
+            |> Seq.map parseLine
+            |> Seq.fold (fun (reg, _, h) i -> processLine reg i h) (Map.empty, 0, 0)
+
+        printfn "part 1 = %d" highest
+        printfn "part 2 = %d" totalHighest
