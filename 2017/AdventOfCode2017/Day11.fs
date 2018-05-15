ebea418f8fe9e47c43d53845daccaf2ef6f70de4
Kenneth Sarmaat <knnth@outlook.com>
2018-05-15 22:37:18 +0200

    make folder point free

--- 2017/AdventOfCode2017/Day11.fs
+++ 2017/AdventOfCode2017/Day11.fs
@@ -16,17 +16,14 @@
 
     let origin = { x = 0; y = 0; z = 0 }
 
-    let move position direction =
-        let newPosition =
-            match direction with
-            | "n" -> { position with y = position.y + 1; z = position.z - 1 }
-            | "ne" -> { position with x = position.x + 1; z = position.z - 1 }
-            | "se" -> { position with x = position.x + 1; y = position.y - 1 }
-            | "s" -> { position with y = position.y - 1; z = position.z + 1 }
-            | "sw" -> { position with x = position.x - 1; z = position.z + 1 }
-            | "nw" -> { position with x = position.x - 1; y = position.y + 1 }
-            | _ -> position
-        newPosition
+    let move position = function
+        | "n" -> { position with y = position.y + 1; z = position.z - 1 }
+        | "ne" -> { position with x = position.x + 1; z = position.z - 1 }
+        | "se" -> { position with x = position.x + 1; y = position.y - 1 }
+        | "s" -> { position with y = position.y - 1; z = position.z + 1 }
+        | "sw" -> { position with x = position.x - 1; z = position.z + 1 }
+        | "nw" -> { position with x = position.x - 1; y = position.y + 1 }
+        | _ -> position
 
     let solve() =
         let result =
